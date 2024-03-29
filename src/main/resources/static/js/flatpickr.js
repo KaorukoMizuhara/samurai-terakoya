a1f5b8474ac182fc80d80e2e805c3c4c8466bb30
KaorukoMizuhara <149221251+KaorukoMizuhara@users.noreply.github.com>
2024-03-29 19:55:52 +0900

    Delete src directory

--- src/main/resources/static/js/flatpickr.js
+++
@@ -1,9 +0,0 @@
-let maxDate = new Date();
-maxDate.setMonth(maxDate.getMonth() + 2);
-
-flatpickr('#fromCheckinDateToCheckinTime', {
-  mode: "single",
-  locale: 'ja',
-  minDate: 'today',
-  maxDate: maxDate
-});
