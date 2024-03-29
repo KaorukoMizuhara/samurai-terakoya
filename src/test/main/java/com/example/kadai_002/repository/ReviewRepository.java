a28ce8e3be12e6dd72e1cd4e46a2978f6ee8b813
KaorukoMizuhara <149221251+KaorukoMizuhara@users.noreply.github.com>
2024-03-29 20:01:30 +0900

    Add files via upload

---
+++ src/test/main/java/com/example/kadai_002/repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.example.kadai_002.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.kadai_002.entity.Review;
+
+public interface ReviewRepository extends JpaRepository<Review, Integer>{
+
+}
+ 
